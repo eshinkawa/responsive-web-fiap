b4c63f2d28301589f58ee479011d3611fb1a4465
Eduardo Shinkawa <eshinkawa@gmail.com>
2023-10-09 09:34:14 -0300

    Aula2

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
-// SingleFileApp.js
+
+// Aula2 - tópicos
+
+// Usar fetch pra puxar dados de api
+// Como mostrar os dados da api na tela
+// display flex no CSS
+
 import React, { useEffect, useState, Suspense } from "react";
-import "./styles.css";
+import './styles.css';
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // declarando o useState
+  const [personagens, setPersonagens] = useState([]);
 
+  // chamar a api
+
+  // um dos react hooks
+  // useEffect = efeito colateral no seu componente
+  // roda o useEffect ao montar a página
+  
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
-      .then((response) => response.json())
-      .then((data) => {
-        setCharacters(data.results);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
+    //efeito colateral
+    getPersonagens()
   }, []);
 
-  return (
-    <div className="single-file-app">
-      <h1>Rick and Morty Characters</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ul className="character-list">
-          {characters.map((character) => (
-            <li key={character.id} className="character-item">
-              <img
-                src={character.image}
-                alt={character.name}
-                className="character-image"
-              />
-              <p className="character-name">{character.name}</p>
-            </li>
-          ))}
-        </ul>
-      </Suspense>
-    </div>
-  );
+  function getPersonagens() {
+    // chamar o fetch do Javascript
+    // fetch significa 'buscar'
+
+    fetch('https://rickandmortyapi.com/api/character')
+      // promise => algo que pode retornar um valor no futuro
+      .then((res) => res.json())
+      // coisas do react => colocar o json no state de personagens
+      .then(resultado => {
+        setPersonagens(resultado.results) 
+      })
+  }
+  // diferenças entra jsx e react
+  return <div className="container">
+    {personagens.map(item => 
+      <div className="item">
+        <img src={item.image} className="imagem"/>
+        <h2>Nome: {item.name}</h2>
+      </div>
+    )} 
+  </div>;
 }
 
 export default App;
+
+
