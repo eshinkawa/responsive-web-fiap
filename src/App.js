8a5d74ad47e2ac8f193863b86df31076ac67aa4e
Eduardo Shinkawa <eshinkawa@gmail.com>
2023-10-09 11:25:53 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
-// SingleFileApp.js
 import React, { useEffect, useState, Suspense } from "react";
-import "./styles.css";
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
-      .then((response) => response.json())
-      .then((data) => {
-        setCharacters(data.results);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <div className="single-file-app">
-      <h1>Rick and Morty Characters</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ul className="character-list">
-          {characters.map((character) => (
-            <li key={character.id} className="character-item">
-              <img
-                src={character.image}
-                alt={character.name}
-                className="character-image"
-              />
-              <p className="character-name">{character.name}</p>
-            </li>
-          ))}
-        </ul>
-      </Suspense>
-    </div>
-  );
+  return <div>Ola</div>;
 }
 
 export default App;
