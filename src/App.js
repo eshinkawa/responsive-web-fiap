ccd512f1e55fcae404a662cb7962189c56b1eda6
Eduardo Shinkawa <eshinkawa@gmail.com>
2023-10-09 11:42:52 -0300

    Chamando API e criando component Personagem

--- src/App.js
+++ src/App.js
@@ -1,49 +1,45 @@
 
 // Aula2 - tópicos
-
-// Usar fetch pra puxar dados de api
-// Como mostrar os dados da api na tela
-// display flex no CSS
+// chamados de API via fetch
+// como dispor items do array no JSX
+// modelo CSS flex
 
 import React, { useEffect, useState, Suspense } from "react";
+import Personagem from './componentes/personagem';
 import './styles.css';
 
+
 function App() {
-  // declarando o useState
-  const [personagens, setPersonagens] = useState([]);
+  // adicionando personagens no estado do react
+  const [personagens, setPersonagens] = useState([])
 
-  // chamar a api
-
-  // um dos react hooks
-  // useEffect = efeito colateral no seu componente
-  // roda o useEffect ao montar a página
-  
+  //chamada de api dentro do useEffect
+  // useEffect = efeito colateral no seu
   useEffect(() => {
-    //efeito colateral
-    getPersonagens()
-  }, []);
+    buscarPersonagens()
+  }, [])
+    
+  
+  function buscarPersonagens(){
 
-  function getPersonagens() {
-    // chamar o fetch do Javascript
-    // fetch significa 'buscar'
-
+    // chamar a url da api
+    // fetch retorna uma promise. PROMISE = algo que pode gerar um valor agora ou no futuro
     fetch('https://rickandmortyapi.com/api/character')
-      // promise => algo que pode retornar um valor no futuro
-      .then((res) => res.json())
-      // coisas do react => colocar o json no state de personagens
-      .then(resultado => {
-        setPersonagens(resultado.results) 
-      })
+     //converter o resultado em json
+     // tranformar o que vem da APi em formato JSON, de forma que a aplicação JS entenda que é de fato um JSON
+     .then(res => res.json())
+     // fazer coisas do react
+     .then(resultado => { 
+      setPersonagens(resultado.results)
+     })
   }
-  // diferenças entra jsx e react
+    
+  // retorna o html, que é na verdade um JSX
   return <div className="container">
-    {personagens.map(item => 
-      <div className="item">
-        <img src={item.image} className="imagem"/>
-        <h2>Nome: {item.name}</h2>
-      </div>
-    )} 
-  </div>;
+    {personagens.map(item =>
+      <Personagem image={item.image} name={item.name} species={item.species} gender={item.gender} />
+    )}
+  </div>
 }
 
 export default App;
---
+++ src/componentes/personagem.js
@@ -0,0 +1,13 @@
+
+import React, { useEffect, useState, Suspense } from "react";
+import '../styles.css';
+
+export default function Personagem({image, name, status, species, gender}) { //recebendo props = propriedades
+    return(<div className="item">
+    <img src={image} className="imagem" />
+    <h2>{name}</h2>
+    <div>{status}</div>
+    <div>{species}</div>
+    <div>{gender}</div>
+  </div>)
+}
